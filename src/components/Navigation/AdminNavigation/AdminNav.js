b54101d7b7b3330629da0edfd8e1598ca78cae35
mathi <mathivanan.palani@aspiresys.com>
2020-08-11 08:25:05 +0530

    wrapping login and register function and add book details and edit and cover update

--- src/components/Navigation/AdminNavigation/AdminNav.js
+++ src/components/Navigation/AdminNavigation/AdminNav.js
@@ -6,27 +6,41 @@ import AdminNavItems from './AdminNavItems/AdminNavItems';
 const AdminNav = (props) =>{
 
     const [menuButton , setMenuButton ] = useState(false);
-    let nav = null;
-    if(menuButton){
-        nav =   <nav className={classes.MobileNavbar}>
-                        <AdminNavItems link="/admin/home" clicked = {()=>setMenuButton(!menuButton)}>Home</AdminNavItems>
-                        <AdminNavItems link="/admin/orders" clicked = {()=>setMenuButton(!menuButton)}>Orders</AdminNavItems>
-                        <AdminNavItems link="/admin/book" clicked = {()=>setMenuButton(!menuButton)}>Books</AdminNavItems>
-                        <AdminNavItems link="/admin/authors" clicked = {()=>setMenuButton(!menuButton)}>Authors</AdminNavItems>
-                        <AdminNavItems link="/admin/publishers" clicked = {()=>setMenuButton(!menuButton)}>Publishers</AdminNavItems>
-                        <AdminNavItems link="/admin/stocks" clicked = {()=>setMenuButton(!menuButton)}>Stocks</AdminNavItems>
-                        <p onClick={()=>{props.logout(); setMenuButton(!menuButton); }} className={classes.Logout}>LogOut</p>
-                </nav>
-    }
+    const Opened = classes.Dropmenu+' '+classes.Open;
+    const Closed = classes.Dropmenu+ ' '+ classes .Close;
+    // let nav = null;
+    // if(menuButton){
+    //     nav =   <nav className={classes.MobileNavbar}>
+    //                     <AdminNavItems link="/admin/home" clicked = {()=>setMenuButton(!menuButton)}>Home</AdminNavItems>
+    //                     <AdminNavItems link="/admin/orders" clicked = {()=>setMenuButton(!menuButton)}>Orders</AdminNavItems>
+    //                     <AdminNavItems link="/admin/book" clicked = {()=>setMenuButton(!menuButton)}>Books</AdminNavItems>
+    //                     <AdminNavItems link="/admin/authors" clicked = {()=>setMenuButton(!menuButton)}>Authors</AdminNavItems>
+    //                     <AdminNavItems link="/admin/publishers" clicked = {()=>setMenuButton(!menuButton)}>Publishers</AdminNavItems>
+    //                     <AdminNavItems link="/admin/stocks" clicked = {()=>setMenuButton(!menuButton)}>Stocks</AdminNavItems>
+    //                     <p onClick={()=>{props.logout(); setMenuButton(!menuButton); }} className={classes.Logout}>LogOut</p>
+    //             </nav>
+    // }
 
     return (
-        <div className={classes.Container}>
-            <div className={classes.MobileNav}>
-                <h1>Readers.com</h1>
-                <div><p onClick={()=>setMenuButton(!menuButton)}>menu</p></div>
+        <>
+            <div>
+                <div className={classes.MobileNav}>
+                    <h1>Readers.com</h1>
+                    <div><p onClick={()=>setMenuButton(!menuButton)}>menu</p></div>
+                </div>
             </div>
-
-            {nav}
+            <div className={menuButton ? Opened : Closed}>
+                <nav className={classes.MobileNavbar}>
+                    <AdminNavItems link="/admin/home" clicked = {()=>setMenuButton(!menuButton)}>Home</AdminNavItems>
+                    <AdminNavItems link="/admin/orders" clicked = {()=>setMenuButton(!menuButton)}>Orders</AdminNavItems>
+                    <AdminNavItems link="/admin/book" clicked = {()=>setMenuButton(!menuButton)}>Books</AdminNavItems>
+                    <AdminNavItems link="/admin/authors" clicked = {()=>setMenuButton(!menuButton)}>Authors</AdminNavItems>
+                    <AdminNavItems link="/admin/publishers" clicked = {()=>setMenuButton(!menuButton)}>Publishers</AdminNavItems>
+                    <AdminNavItems link="/admin/stocks" clicked = {()=>setMenuButton(!menuButton)}>Stocks</AdminNavItems>
+                    <p onClick={()=>{props.logout(); setMenuButton(!menuButton); }} className={classes.Logout}>LogOut</p>
+                </nav>
+            </div>
+            
             <div className={classes.DesktopNav}>
                 <nav className={classes.AdminNavbar}>
                     <AdminNavItems link="/admin/home">Home</AdminNavItems>
@@ -39,7 +53,7 @@ const AdminNav = (props) =>{
                     <p onClick={props.logout} className={classes.Logout}>LogOut</p>
                 </nav>
             </div>
-        </div>
+            </>
     );
 }
 
