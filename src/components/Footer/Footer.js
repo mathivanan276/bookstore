7be4c91bd6919933b27ed2a1af0a74810ee8b72c
mathi <mathivanan.palani@aspiresys.com>
2020-09-03 14:16:35 +0530

    added hosting

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,7 +6,8 @@ const Footer = () => {
     return(
         <div className={classes.Footer}>
             <p>Copyright &copy; 2020 Tamilnadu-BookStore.Pvt Ltd</p>
-            <p>Powered By : <a href='https://about.techmathi.tk'>techmathi</a></p>
+            <p>About Me : <a href='https://about.techmathi.tk'>about.techmathi.tk</a></p>
+            <p><a href='mailto:mathivanan276@gmail.com' >mathivanan276@gmail.com</a></p>
         </div>
     );
 }
